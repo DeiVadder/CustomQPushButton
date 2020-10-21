6d6dd7439f971a8ca02354e8f79d1b7029ebedf0
DeiVadder <Jonas.Hilk@googlemail.com>
2020-10-21 07:53:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include "mybutton.h"
 
 #include <QApplication>
+#include <QDebug>
+
+void receivingFunction(bool mouseHasEntered){
+    qDebug() << "main.cpp: mouseHasEntered" << mouseHasEntered;
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MyButton w;
     w.show();
+
+    QObject::connect(&w, &MyButton::mouseEntered, &receivingFunction);
+
     return a.exec();
 }
